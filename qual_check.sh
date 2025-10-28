b00c3bc1f07eec9422d26bbe765faa6a591cb484
creighton-lewis <creightonlewis01@gmail.com>
2025-10-28 10:43:22 -0500

    edited qual_check to include test.py document

--- qual_check.sh
+++ qual_check.sh
@@ -7,9 +7,9 @@ uv run main.py  -k apache --msfmodule
 uv run main.py  -k apache --nvd
 uv run main.py  -k apache --exploitdb 
 uv run main.py  -k CVE-2001-1002 --exploitdb
-uv run main.py  -k CVV-2001-1002 --exploitdb -o test_file
 uv run nparse.py xml_docs/xml3 
 uv run nparse.py xml_docs/xml4
+uv run test.py 
 
 
 
