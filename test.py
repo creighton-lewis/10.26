a45a57842345714819a09d8f35c1ddfd3eaff8c1
creighton-lewis <creightonlewis01@gmail.com>
2025-10-28 10:45:06 -0500

    imported
    -changed test.py to run subprocess instead of os.system for CVE extraction

--- test.py
+++ test.py
@@ -1,34 +1,47 @@
 import os 
 import sys
 import time
+from pathlib import Path 
 from rich.console import Console 
 console = Console()
 from poc import batch_from_file
 import xml.etree.ElementTree as ET
 from typing import Dict, List, Optional
-file_name = "xml3"
-temp_file = f"{file_name}_temp"
-cve_file = f"{file_name}_temp_cve"
+file_path = sys.argv[1]
+file_name = Path(file_path).stem
+print (file_name)
+temp_file = f"{file_name}_temp" # original nmap report, formatted
+cve_file = f"{temp_file}_cve" #temporary cve file 
 console.print("Running scan...")
 #Run initial script 
 def main():
-    os.system(f"uv run nparse.py xml_docs/{file_name} >> {temp_file}")
-    #os.system(f" sed -E 's/CVE://' {temp_file}")
-    console.print("Here's the scan", style = "bold cyan")
-    with open(f"{temp_file}", 'r') as file:
+    import subprocess
+    os.system(f"uv run nparse.py {file_path} >> {temp_file}") #creates normal looking report 
+    try:
+        os.system(f"uv run main.py -nm {file_path} --all")
+    except:
+        console.print("Unable to run normal nmap scan for services")
+    with open(f"{temp_file}", 'r') as file: #ensures file looks good 
         content = file.read()
         print(content)   
-    result = os.system(f"grep 'CVE' {temp_file} >> {cve_file}")
-    os.system(f"sed -E 's/CVE://' {cve_file}")
-try:
-        cve_output = batch_from_file(f"{cve_file}")
-except:
-        console.print("Fail")
-try:
-            cve_output = os.system(f"uv run poc.py -f {cve_file} >> last_file")
-            
-except:
-            console.print("Fail")
+    #result = os.system(f"grep 'CVE' {temp_file} >> {cve_file}") #exports found cve's to a regular file 
+    #os.remove(temp_file)
+    subprocess.run(['grep', 'CVE', 'temp_file', '>>', 'cve_file'], capture_output=True, text=True)
+    subprocess.run ("sed -i 's/CVE://g' cve_file", shell=True)
+    """ 
+    try:
+        cve_output = os.system(f"uv run poc.py -f {cve_file} >> last_file")
+    except:
+                console.print("Fail")
+    try:
+            os.remove(temp_file)
+    except:
+            print("unable to remove")
+    try:
+        os.remove(cve_file)
+    except: 
+            print("can't remove")
+    """
 main()
 
 """
