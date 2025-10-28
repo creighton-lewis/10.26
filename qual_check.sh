a7f3b0b3c1a53a76ac0b517142d93ed00274e1c8
creighton-lewis <creightonlewis01@gmail.com>
2025-10-28 18:26:24 -0500

    added output and product version to quality check

--- qual_check.sh
+++ qual_check.sh
@@ -9,7 +9,10 @@ uv run main.py  -k apache --exploitdb
 uv run main.py  -k CVE-2001-1002 --exploitdb
 uv run nparse.py xml_docs/xml3 
 uv run nparse.py xml_docs/xml4
-uv run test.py 
+uv run poc.py -f
+uv run main.py -kv apache_2.0 --exploitdb 
+uv run main.py -kv apache_2.0 -o apache_2.0 
+uv run main.py -k apache --msfmodule #fix msfmodule & formatting
 
 
 
